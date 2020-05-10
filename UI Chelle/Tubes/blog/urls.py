b2381cc018a9e356e048e8dfb9cf9b2f1ca2610f
Your Name <you@example.com>
2020-05-10 22:38:33 +0700

    terbaru

--- UI Chelle/Tubes/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# from fontawesome.fields import IconField
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('query/', views.query, name="query"),
-    path('toprank/', views.toprank, name="toprank"),
-]
