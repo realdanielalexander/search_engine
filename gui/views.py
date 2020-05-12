a7fce8a8cfca11fe0462c41ba8af0a0ddee63f02
unknown <realdanielalexander@gmail.com>
2020-05-12 13:59:37 +0700

    final

--- gui/views.py
+++ gui/views.py
@@ -387,8 +387,8 @@ def tfidf(kueri, limit):
 
     query = kueri
     text, data = preprocess(query)
-    
-    z=1
+
+    z = 1
     count = 1
     N = 500
     plus = 0
@@ -420,44 +420,43 @@ def tfidf(kueri, limit):
     counter = 0
 
     starts = timeit.default_timer()
-    
+
     for q in data:
-        if data[counter]==data[counter-1]:
-            plus+=1
+        if data[counter] == data[counter-1]:
+            plus += 1
             dict_f[q] = plus
         else:
             plus = 1
             dict_f[q] = plus
-        counter+=1
+        counter += 1
 
     for q in data:
-        for i in range (0,max_range):
+        for i in range(0, max_range):
             ni = dict_f[q]
             start = 2
             end = 5
             count = 1
             if (term[i] == q):
-                for y in range (0,int(len(doc[i])/7)):
-                    if(int(len(doc[i])/7)!=1):
+                for y in range(0, int(len(doc[i])/7)):
+                    if(int(len(doc[i])/7) != 1):
                         if(doc[i][start:end] != doc[i][(start-7):(end-7)]):
-                            ni+=1
-                            dict_f[q +' doc ' + doc[i][start:end]]=count
+                            ni += 1
+                            dict_f[q + ' doc ' + doc[i][start:end]] = count
                         else:
-                            dict_f[q + ' doc' + doc[i][start:end]]=count
-                            count+=1
-                            ni=1
+                            dict_f[q + ' doc' + doc[i][start:end]] = count
+                            count += 1
+                            ni = 1
                     else:
-                        dict_f[q +' doc ' + doc[i][start:end]]=count
-                        ni+=1
+                        dict_f[q + ' doc ' + doc[i][start:end]] = count
+                        ni += 1
 
                     start = start+7
                     end = end+7
 
-                dict_idf[q] = math.log((1+N/ni),10)
+                dict_idf[q] = math.log((1+N/ni), 10)
 
     for key in dict_f:
-        dict_tf[key] = 1+math.log(dict_f[key],10)
-
+        dict_tf[key] = 1+math.log(dict_f[key], 10)
 
     for key in dict_tf:
         for key2 in dict_idf:
@@ -467,76 +466,76 @@ def tfidf(kueri, limit):
                 dict_w[key] = dict_tf[key]*dict_idf[key2]
 
     for key in dict_w:
-        if(key[len(key)-7:len(key)-4] == 'doc' ):
-            dict_dj[key[len(key)-7:len(key)]] = (pow(dict_w[key],2))
+        if(key[len(key)-7:len(key)-4] == 'doc'):
+            dict_dj[key[len(key)-7:len(key)]] = (pow(dict_w[key], 2))
             list_angka.append(dict_dj[key[len(key)-7:len(key)]])
             list_nama.append(key[len(key)-7:len(key)])
         else:
-            dict_dj[key] = pow(dict_w[key],2)
+            dict_dj[key] = pow(dict_w[key], 2)
             list_nama.append('q')
             list_angka.append(dict_dj[key])
 
-    for i in range(0,len(list_nama)):
-        for x in range(i+1,len(list_nama)):
-            if (list_nama[i]==list_nama[x]):
+    for i in range(0, len(list_nama)):
+        for x in range(i+1, len(list_nama)):
+            if (list_nama[i] == list_nama[x]):
 
                 list_final.append(math.sqrt(list_angka[i]+list_angka[x]))
                 list_finalnama.append(list_nama[i])
                 # list_angka[i]=0
                 # list_angka[x] = 0
 
-    for i in range(0,len(list_nama)):
-        if list_angka[i]!=0:
+    for i in range(0, len(list_nama)):
+        if list_angka[i] != 0:
             list_final.append(math.sqrt(list_angka[i]))
             list_finalnama.append(list_nama[i])
 
-    for i in range(0,len(list_final)):
+    for i in range(0, len(list_final)):
         dict_dj2[list_finalnama[i]] = list_final[i]
-
 
     for key in dict_w:
         list_wnama.append(key)
         list_wangka.append(dict_w[key])
 
-    for i in range(0,len(list_wnama)):
-        for x in range(i+1,len(list_wnama)):
-            if(list_wnama[i]==list_wnama[x][0:len(list_wnama[x])-8]):
+    for i in range(0, len(list_wnama)):
+        for x in range(i+1, len(list_wnama)):
+            if(list_wnama[i] == list_wnama[x][0:len(list_wnama[x])-8]):
                 list_simnama.append(list_wnama[x][0:len(list_wnama[x])-8])
                 list_simdoc.append(list_wnama[x][len(list_wnama[x])-7:])
                 list_sim.append(list_wangka[x])
                 list_wangka[x] = 0
 
-    for i in range(0,len(list_wangka)):
-        if(list_wangka[i]!=0):
+    for i in range(0, len(list_wangka)):
+        if(list_wangka[i] != 0):
             list_simq.append(list_wangka[i])
             list_simqnama.append(list_wnama[i])
 
-    for i in range(0,len(list_sim)):
-        for y in range(0,len(list_simq)):
-            if(list_simnama[i]==list_simqnama[y]):
+    for i in range(0, len(list_sim)):
+        for y in range(0, len(list_simq)):
+            if(list_simnama[i] == list_simqnama[y]):
                 list_finalsim.append(list_sim[i]*list_simq[y])
 
-    for i in range(0,len(list_finalsim)):
-        for x in range(i+1,len(list_simdoc)):
-            if list_simdoc[i]==list_simdoc[x]:
+    for i in range(0, len(list_finalsim)):
+        for x in range(i+1, len(list_simdoc)):
+            if list_simdoc[i] == list_simdoc[x]:
                 list_finalsim2.append(list_finalsim[i]+list_finalsim[x])
                 list_finalsim[i] = 0
                 list_finalsim[x] = 0
                 list_finalsim2doc.append(list_simdoc[i])
 
-    for i in range(0,len(list_finalsim)):
-        if(list_finalsim[i]!=0):
+    for i in range(0, len(list_finalsim)):
+        if(list_finalsim[i] != 0):
             list_finalsim2.append(list_finalsim[i])
             list_finalsim2doc.append(list_simdoc[i])
 
-    for i in range(0,len(list_finalsim2)):
+    for i in range(0, len(list_finalsim2)):
         try:
-            dict_sim[list_finalsim2doc[i]] = list_finalsim2[i]/(dict_dj2[list_finalsim2doc[i]]*dict_dj2['q'])
+            dict_sim[list_finalsim2doc[i]] = list_finalsim2[i] / \
+                (dict_dj2[list_finalsim2doc[i]]*dict_dj2['q'])
         except:
             print("0")
 
     stop = timeit.default_timer()
-    
+
     return dict_sim, stop-starts
 
 
@@ -549,8 +548,8 @@ def tfidf_ascending(kueri, limit):
 
     query = kueri
     text, data = preprocess(query)
-    
-    z=1
+
+    z = 1
     count = 1
     N = 500
     plus = 0
@@ -582,44 +581,43 @@ def tfidf_ascending(kueri, limit):
     counter = 0
 
     starts = timeit.default_timer()
-    
+
     for q in data:
-        if data[counter]==data[counter-1]:
-            plus+=1
+        if data[counter] == data[counter-1]:
+            plus += 1
             dict_f[q] = plus
         else:
             plus = 1
             dict_f[q] = plus
-        counter+=1
+        counter += 1
 
     for q in data:
-        for i in range (0,max_range):
+        for i in range(0, max_range):
             ni = dict_f[q]
             start = 2
             end = 5
             count = 1
             if (term[i] == q):
-                for y in range (0,int(len(doc[i])/7)):
-                    if(int(len(doc[i])/7)!=1):
+                for y in range(0, int(len(doc[i])/7)):
+                    if(int(len(doc[i])/7) != 1):
                         if(doc[i][start:end] != doc[i][(start-7):(end-7)]):
-                            ni+=1
-                            dict_f[q +' doc ' + doc[i][start:end]]=count
+                            ni += 1
+                            dict_f[q + ' doc ' + doc[i][start:end]] = count
                         else:
-                            dict_f[q + ' doc' + doc[i][start:end]]=count
-                            count+=1
-                            ni=1
+                            dict_f[q + ' doc' + doc[i][start:end]] = count
+                            count += 1
+                            ni = 1
                     else:
-                        dict_f[q +' doc ' + doc[i][start:end]]=count
-                        ni+=1
+                        dict_f[q + ' doc ' + doc[i][start:end]] = count
+                        ni += 1
 
                     start = start+7
                     end = end+7
 
-                dict_idf[q] = math.log((1+N/ni),10)
+                dict_idf[q] = math.log((1+N/ni), 10)
 
     for key in dict_f:
-        dict_tf[key] = 1+math.log(dict_f[key],10)
-
+        dict_tf[key] = 1+math.log(dict_f[key], 10)
 
     for key in dict_tf:
         for key2 in dict_idf:
@@ -629,76 +627,76 @@ def tfidf_ascending(kueri, limit):
                 dict_w[key] = dict_tf[key]*dict_idf[key2]
 
     for key in dict_w:
-        if(key[len(key)-7:len(key)-4] == 'doc' ):
-            dict_dj[key[len(key)-7:len(key)]] = (pow(dict_w[key],2))
+        if(key[len(key)-7:len(key)-4] == 'doc'):
+            dict_dj[key[len(key)-7:len(key)]] = (pow(dict_w[key], 2))
             list_angka.append(dict_dj[key[len(key)-7:len(key)]])
             list_nama.append(key[len(key)-7:len(key)])
         else:
-            dict_dj[key] = pow(dict_w[key],2)
+            dict_dj[key] = pow(dict_w[key], 2)
             list_nama.append('q')
             list_angka.append(dict_dj[key])
 
-    for i in range(0,len(list_nama)):
-        for x in range(i+1,len(list_nama)):
-            if (list_nama[i]==list_nama[x]):
+    for i in range(0, len(list_nama)):
+        for x in range(i+1, len(list_nama)):
+            if (list_nama[i] == list_nama[x]):
 
                 list_final.append(math.sqrt(list_angka[i]+list_angka[x]))
                 list_finalnama.append(list_nama[i])
                 # list_angka[i]=0
                 # list_angka[x] = 0
 
-    for i in range(0,len(list_nama)):
-        if list_angka[i]!=0:
+    for i in range(0, len(list_nama)):
+        if list_angka[i] != 0:
             list_final.append(math.sqrt(list_angka[i]))
             list_finalnama.append(list_nama[i])
 
-    for i in range(0,len(list_final)):
+    for i in range(0, len(list_final)):
         dict_dj2[list_finalnama[i]] = list_final[i]
-
 
     for key in dict_w:
         list_wnama.append(key)
         list_wangka.append(dict_w[key])
 
-    for i in range(0,len(list_wnama)):
-        for x in range(i+1,len(list_wnama)):
-            if(list_wnama[i]==list_wnama[x][0:len(list_wnama[x])-8]):
+    for i in range(0, len(list_wnama)):
+        for x in range(i+1, len(list_wnama)):
+            if(list_wnama[i] == list_wnama[x][0:len(list_wnama[x])-8]):
                 list_simnama.append(list_wnama[x][0:len(list_wnama[x])-8])
                 list_simdoc.append(list_wnama[x][len(list_wnama[x])-7:])
                 list_sim.append(list_wangka[x])
                 list_wangka[x] = 0
 
-    for i in range(0,len(list_wangka)):
-        if(list_wangka[i]!=0):
+    for i in range(0, len(list_wangka)):
+        if(list_wangka[i] != 0):
             list_simq.append(list_wangka[i])
             list_simqnama.append(list_wnama[i])
 
-    for i in range(0,len(list_sim)):
-        for y in range(0,len(list_simq)):
-            if(list_simnama[i]==list_simqnama[y]):
+    for i in range(0, len(list_sim)):
+        for y in range(0, len(list_simq)):
+            if(list_simnama[i] == list_simqnama[y]):
                 list_finalsim.append(list_sim[i]*list_simq[y])
 
-    for i in range(0,len(list_finalsim)):
-        for x in range(i+1,len(list_simdoc)):
-            if list_simdoc[i]==list_simdoc[x]:
+    for i in range(0, len(list_finalsim)):
+        for x in range(i+1, len(list_simdoc)):
+            if list_simdoc[i] == list_simdoc[x]:
                 list_finalsim2.append(list_finalsim[i]+list_finalsim[x])
                 list_finalsim[i] = 0
                 list_finalsim[x] = 0
                 list_finalsim2doc.append(list_simdoc[i])
 
-    for i in range(0,len(list_finalsim)):
-        if(list_finalsim[i]!=0):
+    for i in range(0, len(list_finalsim)):
+        if(list_finalsim[i] != 0):
             list_finalsim2.append(list_finalsim[i])
             list_finalsim2doc.append(list_simdoc[i])
 
-    for i in range(0,len(list_finalsim2)):
+    for i in range(0, len(list_finalsim2)):
         try:
-            dict_sim[list_finalsim2doc[i]] = list_finalsim2[i]/(dict_dj2[list_finalsim2doc[i]]*dict_dj2['q'])
+            dict_sim[list_finalsim2doc[i]] = list_finalsim2[i] / \
+                (dict_dj2[list_finalsim2doc[i]]*dict_dj2['q'])
         except:
             print("0")
 
     stop = timeit.default_timer()
-    
+
     return dict_sim, stop-starts
 
 
@@ -769,7 +767,7 @@ def toprank_ascending(request):
             for key, value in documents.items():
                 result_dict.append((key[4:], value))
                 result_sorted = sorted(
-                    result_dict, key=lambda x: x[0], reverse=True)
+                    result_dict, key=lambda x: x[0])
 
             for key, value in result_sorted:
                 dictionary = dict()
